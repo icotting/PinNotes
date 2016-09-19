a629932b55eff5b32f65517926878a61dcdd803e
Ian Cottingham <icottingham2@unl.edu>
2016-09-19 12:47:00 -0500

    Fixed build script bug

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 dotnet restore
 dotnet test test/Unit/PinNotes.Accessors.Domain.Tests/
 
-dotnet build src/0 - Clients/PinNotes.Web -c Release
+dotnet build "src/0 - Clients/PinNotes.Web" -c Release
